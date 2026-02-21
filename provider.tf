f760dd681188db4c4d204c2a91f6a0b14cc7de5c
valencloud <tinobracex640@gmail.com>
2026-02-21 21:26:46 +0100

    configure and added s3 bucket, versioning and dynamo db to the main project file

--- provider.tf
+++ provider.tf
@@ -13,3 +13,13 @@ provider "aws" {
   region = "us-east-1"
   profile = "terraform-ssm"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "tinobrace-terraform-state-bucket-2026"
+    key            = "ssm-lab/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+    encrypt        = true
+  }
+}
